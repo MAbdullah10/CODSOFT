a63d53eb43a403e75dc51f440aa33f05f678350b
MAbdullah10 <120081761+MAbdullah10@users.noreply.github.com>
2023-08-31 00:57:01 +0500

    Add files via upload

--- Student Grade Calculator/Test.java
+++ Student Grade Calculator/Test.java
@@ -1,1 +1,32 @@
+import java.util.Scanner;
+public class Test
+{
+    public static void main(String[] args) {
+        StudentGradeCalculator sgc = new StudentGradeCalculator();
+        Scanner s = new Scanner(System.in);
 
+        sgc.inputSubjectNamesAndMarks(s);
+
+        int total_marks = 0;
+        int marks;
+        for (int j = 0; j < sgc.total_subjects; j++) {
+            total_marks = total_marks + sgc.subjectMarks[j];
+        }
+        {
+            for (int i = 0; i < sgc.total_subjects; i++) {
+                marks = sgc.subjectMarks[i];
+                sgc.calculateGradePoints(marks);
+            }
+        }
+        sgc.WeightedAverage();
+        sgc.calculateGPA();
+        sgc.grade_calculation();
+        sgc.subjectsDisplay();
+        sgc.display(total_marks);
+
+        sgc.HighestAndLowestMarks();
+        sgc.PassedAndFailedSubjects();
+        sgc.PassOrFail();
+        sgc.AdditionalDisplay();
+    }
+}
